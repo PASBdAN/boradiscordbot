b0c90eed7470bfcfac861d40d189122b80a19cd8
Petronilo Bion <petroniloneto.am@hotmail.com>
2021-09-14 04:40:33 -0300

    adding test connection line

--- database/client.py
+++ database/client.py
@@ -6,6 +6,7 @@ class Client():
     def __init__(self):
         # self.conn = sqlite3.connect(db_name)
         self.conn = psycopg2.connect(os.environ['DATABASE_URL'])
+        # self.conn = psycopg2.connect("postgres://iahdcmqnelbrfy:6f9a9833e0922ea8b9729e5701a706664c6b1308df8207257e16ff502059da90@ec2-54-83-137-206.compute-1.amazonaws.com:5432/dbful1aoklvin4")
         self.cursor = self.conn.cursor()
 
     def commit_db(self):
