4debf561149ca41d3797cabd9400b6823683038f
Petronilo Bion <petroniloneto.am@hotmail.com>
2021-09-14 03:24:27 -0300

    removing unused columns, fixing primary key for postgres use

--- database/schemas/guilds_schema.sql
+++ database/schemas/guilds_schema.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS guilds (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    --id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     guild_id BIGINT UNIQUE,
-    prefix TEXT,
-    activity TEXT,
-    activity_timer INTEGER
+    prefix TEXT
+    --activity TEXT,
+    --activity_timer INTEGER
 );
