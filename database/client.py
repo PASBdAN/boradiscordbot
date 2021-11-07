01727f87cac4e8adf829bfd47b6b2e50d5c67c7f
Petronilo Bion <petroniloneto.am@hotmail.com>
2021-11-07 14:59:35 -0400

    adding hidden auth variables

--- database/client.py
+++ database/client.py
@@ -1,12 +1,13 @@
 # import sqlite3
 import psycopg2
 import os
+# from config.config import DATABASE_URL
 
 class Client():
     def __init__(self):
         # self.conn = sqlite3.connect(db_name)
         self.conn = psycopg2.connect(os.environ['DATABASE_URL'])
-        # self.conn = psycopg2.connect("postgres://iahdcmqnelbrfy:6f9a9833e0922ea8b9729e5701a706664c6b1308df8207257e16ff502059da90@ec2-54-83-137-206.compute-1.amazonaws.com:5432/dbful1aoklvin4")
+        # self.conn = psycopg2.connect(DATABASE_URL)
         self.cursor = self.conn.cursor()
 
     def commit_db(self):
