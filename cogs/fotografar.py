7f8ff871289d208050ce12c804ceaa9dadbdae88
Petronilo Bion <petroniloneto.am@hotmail.com>
2022-01-01 17:28:57 -0400

    doing more testing on deploy

--- cogs/fotografar.py
+++ cogs/fotografar.py
@@ -11,13 +11,17 @@ class Fotografar(commands.Cog):
     # EVENT LISTENERS
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
-        self.channel = self.bot.get_channel('851446887210942484')
+        # self.channel = self.bot.get_channel('851446887210942484')
         # 851796006260965386
-        # self.channel = self.bot.get_channel(851796006260965386)
+        # self.channel = self.bot.get_channel(177808798117920768)
+        # 888088357463285821
+        # self.channel = self.bot.get_channel(888088357463285821)
+        # 736746755986554951
+        self.channel = self.bot.get_channel(736746755986554951)
+        print(self.channel)
         if reaction.message.channel.id == self.channel.id:
-            print("TESTE")
-            # Role = discord.utils.get(user.guild.roles, name="outro")
             Role = user.guild.get_role(926608766609289267)
+            # Role = user.guild.get_role(926942005316190289)
             print(Role)
             await user.add_roles(Role)
 
