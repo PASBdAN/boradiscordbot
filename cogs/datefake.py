8a53c2961f9228a0802216150aa6c664b0931e67
PASBdAN <petroniloneto.am@hotmail.com>
2022-05-23 03:57:09 -0300

    pre-heroku 5

--- cogs/datefake.py
+++ cogs/datefake.py
@@ -19,8 +19,8 @@ class Datefake(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.module_name = "Datefake"
-        # self.role_id = 978153725711503430
-        self.role_id = 978155909987577876 # server de teste
+        self.role_id = 978153725711503430
+        # self.role_id = 978155909987577876 # server de teste
 
     # Inicialização do bot
     @commands.Cog.listener()
